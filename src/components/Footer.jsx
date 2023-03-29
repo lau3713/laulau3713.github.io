320766b3810d8b3bf6d6ef48a0df6af636690e5f
lau3713 <laurie.tis49@gmail.com>
2023-03-29 14:05:29 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
+import { FaFacebookF } from 'react-icons/fa'
+import { BsInstagram } from 'react-icons/bs'
+import { IoIosMailOpen } from 'react-icons/io'
+import '../styles/FooterStyle.css'
+import '../styles/VariablesStyle.css'
+
 function Footer() {
   return ( 
     <div className="footer">
-      <div className='header-logo'>
+      <div className='footer-menu'>
+        <nav>
+          <span>
+            <a href='/qui-sommes-nous'>QUI SOMMES NOUS ?</a>
+          </span>
+          <span>
+            <a href='/produit'>NOTRE PRODUIT</a>
+          </span>
+          <span>
+            <a href='/contact'>CONTACTEZ-NOUS</a>
+          </span>
+        </nav>
+      </div>
+      <div className='footer-logo'>
         <img src='/assets/img/logo.svg'/>
       </div>
+
+      <div className='social-media'>
+        <p>Suivez-nous sur les réseaux sociaux
+pour découvrir notre aspirateur en action !</p>
+      <div>
+        <FaFacebookF size={30} />
+        <BsInstagram size={30} />
+        <IoIosMailOpen size={30} />
+      </div>
+      </div>
+      <p></p>
 
     </div>
    );
